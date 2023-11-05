cc5845793986a799dab7ccbe1cffb7e9bebb51c2
VitaliyBondar <ipz234_bvo@student.ztu.edu.ua>
2023-11-05 16:34:56 +0200

    Створений консольний проект  LinearExpressions1

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,20 @@
+﻿double a,b,c,d,x,y,z,r;
+Console.WriteLine("Введiть значення a =");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введiть значення b =");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введiть значення c =");
+c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введiть значення d =");
+d = Convert.ToDouble(Console.ReadLine());
+if (c * d == 0 || c - d == 0) Console.WriteLine("Знаменник виразу змінної {x} не може бути нулю");
+else { 
+x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - a * a / (b * b);
+y = 5 * (a + b) * (c - d) / (1.0/ 2 * c) + d * d * (a * a - b * b) / (b - a);
+z = (Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b);
+r = (1 / 2.0 * a + 3.0 / 4 * b - 7.0 / 5) / (3 * c + 1) + (1 / (a - c));
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(r);
+}
