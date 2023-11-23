eb88235a2e2784e16184ec76e649d59cbf2ae759
VitaliyBondar <ipz234_bvo@student.ztu.edu.ua>
2023-11-23 17:12:48 +0200

    Додано безліч змін у коді

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -1,4 +1,9 @@
-﻿double a, b, c, d, x, y, z, r;
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+double a, b, c, d, x, y, z, r;
 
 Console.WriteLine("Введiть значення,якi пропонує ввести додаток,щоб вирахувати вирази рiзних змiн");
 
@@ -13,16 +18,16 @@ d = Convert.ToDouble(Console.ReadLine());
 
 x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - a * a / (b * b);
 y = 5 * (a + b) * (c - d) / (1.0 / 2 * c) + d * d * (a * a - b * b) / (b - a);
-z = (Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b);
+z = Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b) / (5 * a + 3 * b);
 r = (1 / 2.0 * a + 3.0 / 4 * b - 7.0 / 5) / (3 * c + 1) + (1 / (a - c));
 
-if (c * d != 0 && c - d != 0 && b != 0)
+if (c * d != 0 || c - d != 0 || b != 0)
 {
     Console.WriteLine("Значення змiнної х ="); Console.WriteLine(x);
 }
 else Console.WriteLine("Знаменник виразу змiнної {x} не може дорівнювати нулю");
 
-if (c != 0 && b != a)
+if (c != 0 || b != a)
 {
     Console.WriteLine("Значення змiнної y ="); Console.WriteLine(y);
 }
@@ -34,8 +39,8 @@ if (5 * a + 3 * b != 0)
 }
 else Console.WriteLine("Знаменник виразу змiнної {z} не може дорівнювати нулю");
 
-if (3 * c + 1 != 0 && a - c != 0)
+if (3 * c + 1 != 0 || a - c != 0)
 {
-    Console.WriteLine("Значення змiнної r ="); Console.WriteLine(r);
+    Console.WriteLine($"Значення змiнної r = {r}"); 
 }
 else Console.WriteLine("Знаменник виразу змiнної {r} не може дорівнювати нулю");
--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -13,11 +13,11 @@ b = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введiть значення a =");
 a = Convert.ToDouble(Console.ReadLine());
 
-z1 = (m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n) / (Math.Sqrt(Math.Pow(m, 3) * n + n * m + m * m - m));
+z1 = (m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + m * m - m);
 z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
 y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
 
-if (Math.Sqrt(Math.Pow(m, 3) * n + n * m + m * m - m) != 0)
+if (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + m * m - m != 0 || Math.Pow(m, 3) * n >= 0 || n >= 0 || m >= 0)
 {
     Console.WriteLine("Значення змiнної z1 ="); Console.WriteLine(z1);
 }
